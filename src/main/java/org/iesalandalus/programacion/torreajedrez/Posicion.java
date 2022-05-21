b8931d736dcc6e9d510ab5edeadd74613b4d76f5
Adan <Adan@DESKTOP-2T5PP11>
2022-05-21 11:49:50 +0200

    6. Posicion Get y Set

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -12,5 +12,34 @@ public class Posicion {
 	
 private int fila;
 private char columna;
+//Declaramos los metodos
+public int getFila() {
+	return this.fila;
+}
+
+private void setFila(int fila) {
+	if (fila >= 1 && fila <= 8) {
+		this.fila = fila;
+	} else {
+		
+		throw new IllegalArgumentException("ERROR: Fila no válida, tiene que ser entre .");
+
+	}
+}
+
+public char getColumna() {
+	return this.columna;
+}
+
+private void setColumna(char columna) {
+	if (columna == 'a' ||columna == 'b' || columna == 'c' ||columna == 'd' ||
+		columna == 'e' ||columna == 'f' ||columna == 'g' ||columna == 'h') {
+		this.columna = columna;
+	} else {
+		
+		throw new IllegalArgumentException("ERROR: Columna no válida,tiene que ser entre a y h");
+
+	}
+}
 
 }
