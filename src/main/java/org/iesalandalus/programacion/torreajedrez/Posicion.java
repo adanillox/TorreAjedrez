7bf93256ffcff1849a23d4dad7dad65ebc8748ee
Adan <Adan@DESKTOP-2T5PP11>
2022-05-21 11:28:09 +0200

    5. Posicion Atributos

---
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.iesalandalus.programacion.torreajedrez;
+
+/**
+ * @author Adan
+ *
+ */
+//Declaramos los atributos
+public class Posicion {
+	
+private int fila;
+private char columna;
+
+}
