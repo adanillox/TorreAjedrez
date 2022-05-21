acdb675ef52a28771b3190a53cef767573784951
Adan <Adan@DESKTOP-2T5PP11>
2022-05-21 11:20:30 +0200

    4. Enum Direccion

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Color.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Color.java
@@ -10,7 +10,7 @@ package org.iesalandalus.programacion.torreajedrez;
 public enum Color {
 	//Todos atributos que pongamos entre los corchetes seran de tipo
 	// Publico, estatico y final,
-	//es decir, que sera visto para todos el valor no cambia y es una constante
+	//es decir, que sera visto para todos, el valor no cambia y es una constante
   BLANCO, NEGRO
   //El constructor por defecto sin parametros se crea sin necesidad de crearlo a mano
 }
---
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Direccion.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.iesalandalus.programacion.torreajedrez;
+
+/**
+ * @author Adan
+ *
+ */
+public enum Direccion {
+	/*
+	   Todos atributos que pongamos entre los corchetes seran de tipo
+	   Publico, estatico y final,
+	   es decir, que sera visto para todos, el valor no cambia y es una constante
+	 */
+	ARRIBA,ABAJO, IZQUIERDA, DERECHA,ENROQUE_CORTO, ENROQUE_LARGO
+	 //El constructor por defecto sin parametros se crea sin necesidad de crearlo a mano
+}
