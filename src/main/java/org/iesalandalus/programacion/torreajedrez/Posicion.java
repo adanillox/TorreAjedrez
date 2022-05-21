f879028950fd52aadc62ef7b102dfda23cce6bf2
Adan <Adan@DESKTOP-2T5PP11>
2022-05-21 16:11:49 +0200

    9. Posicion equals y HashCode

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -3,6 +3,8 @@
  */
 package org.iesalandalus.programacion.torreajedrez;
 
+import java.util.Objects;
+
 /**
  * @author Adan
  *
@@ -56,6 +58,23 @@ private void setColumna(char columna) {
 		throw new IllegalArgumentException("ERROR: Columna no valida.");
 
 	}
+}
+
+@Override
+public int hashCode() {
+	return Objects.hash(columna, fila);
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Posicion other = (Posicion) obj;
+	return columna == other.columna && fila == other.fila;
 }
 
 }
