a888ae42a61a389a00a1a782325bda6e6d428416
Adan <Adan@DESKTOP-2T5PP11>
2022-05-22 12:54:12 +0200

    20. Main (11 y 12) + Main principal

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class MainApp {
@@ -32,7 +34,7 @@ public class MainApp {
 	private static int elegirOpcion() {
 		int opcion;
 		do {
-			System.out.print(" Elija una opción: ");
+			System.out.print(" Elija una opcion: ");
 			opcion = Entrada.entero();
 		} while (opcion < 1 || opcion > 5);
 		return opcion;
@@ -40,11 +42,10 @@ public class MainApp {
 	
 	private static  Color elegirColor() {
 		int elegir;
+		do {
 		System.out.println("Elija un color");
 		System.out.println(" 1- Blanco");
 		System.out.println(" 2- Negro ");
-
-		do {
 			elegir = Entrada.entero();
 		} while (elegir != 1 && elegir != 2);
 
@@ -56,35 +57,139 @@ public class MainApp {
 	}
 	
 	private static char elegirColumnaInicial() {
-		
+		char ColumnaIni;
+		do {
+			System.out.print("Elige  la columna inicial (a o h): ");
+			ColumnaIni = Entrada.caracter();
+		} while (ColumnaIni != 'a' && ColumnaIni != 'h');
+		return ColumnaIni;
 	}
 	
 	private static void mostrarMenuDirecciones() {
-		
+		System.out.println("Menu de direcciones");
+		System.out.println("1. Arriba");
+		System.out.println("2. Abajo");
+		System.out.println("3. Izquierda");
+		System.out.println("4. Derecha");
+		System.out.println("5. Enroque Corto");
+		System.out.println("6. Enroque Largo");
+	
 	}
 	
 	private static Direccion elegirDireccion() {
 		
+		int opcion;
+		do {
+			System.out.print(" Elija una opcion: ");;
+			opcion = Entrada.entero();
+
+		} while (opcion < 1 || opcion > 6);
+
+		Direccion direccion=null ;
+		switch (opcion) {
+		case 1:
+			direccion = Direccion.ARRIBA;
+			break;
+		case 2:
+			direccion = Direccion.ABAJO;
+			break;
+		case 3:
+			direccion = Direccion.IZQUIERDA;
+			break;
+		case 4:
+			direccion = Direccion.DERECHA;
+			break;
+		case 5:
+			direccion = Direccion.ENROQUE_CORTO;
+			break;
+		case 6:
+			direccion = Direccion.ENROQUE_LARGO;
+			break;
+		default:
+
+		}
+		return direccion;
 	}
 	
 	private static void crearTorreDefecto() {
-		
+		torre = new Torre();
+		System.out.println("Se creo la torre por defecto en la posicion 8h de color negro.");
 	}
 	
 	
 	private static void crearTorreColor() {
-		
+		Color color = elegirColor();
+		torre = new Torre(color);
+		System.out.println("Se creo la torre por defecto a partir del color.");
+	
 	}
 	
 	
 	private static void crearTorreColorColumna() {
+		char columna = elegirColumnaInicial();
+		Color color = elegirColor();
+		torre = new Torre(color, columna);
+		System.out.println("Se creo una torre a partir del color y columna seleccionados.");
 		
 	}
 	
-	private static void mover() {
+	private static void mover() throws OperationNotSupportedException {
+		Direccion direccion = elegirDireccion();
+
+		if (direccion != Direccion.ENROQUE_CORTO && direccion != Direccion.ENROQUE_LARGO) {
+			System.out.print("¿Cuántos pasos quieres que de la torre?: ");
+			int pasos = Entrada.entero();
+
+			try {
+				torre.mover(direccion, pasos);
+			} catch (OperationNotSupportedException | NullPointerException e) {
+				System.out.println(e.getMessage());
+			}
 		
+		} else {
+			try {
+				torre.enrocar(direccion);
+				if (direccion == Direccion.ENROQUE_CORTO ) {
+				System.out.print("El enroque corto ha sido correcto;)\n");
+				} else {
+					System.out.print("El enroque largo ha sido correcto ;)\n");
+				}
+				
+			} catch (OperationNotSupportedException | NullPointerException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	
+		
+
+		System.out.print("Indique el número de pasos a dar");
+		int pasos = Entrada.entero();
+		torre.mover(direccion, pasos);
+		System.out.println("Movimiento realizado correctamente");
 	}
 	
+	private static void ejecutarOpcion(int opcion) throws OperationNotSupportedException {
+		switch (opcion) {
+			case 1:
+				crearTorreDefecto();
+				break;
+			case 2:
+				crearTorreColor();
+				break;
+			case 3:
+				crearTorreColorColumna();
+				break;
+			case 4:
+				if (torre != null) {
+					mover();
+				} else {
+					System.out.println("No hay ninguna torre creada.");
+				}
+				break;
+			default:
+				break;
+		}
+	}
 	
 	
 }
